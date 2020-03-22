b391521c20e96198ceb15c35a12e0ddffbaba1fe
ValentinMes <valentin.mesaros@gmail.com>
2020-03-22 16:01:40 +0100

    clean up and refactor

--- src/main/java/me/serendipify/appengine/experiments/HelloAppEngine.java
+++ src/main/java/me/serendipify/appengine/experiments/HelloAppEngine.java
@@ -1,95 +1,98 @@
 package me.serendipify.appengine.experiments;
 
 import java.io.IOException;
-import java.security.GeneralSecurityException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
+
+/*
+import java.security.GeneralSecurityException;
+*/
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/*
 import com.google.api.client.auth.oauth2.AuthorizationCodeFlow;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.appengine.http.UrlFetchTransport;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+*/
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeRequestUrl;
+
+/*
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
+*/
 
 @WebServlet(
-    name = "HelloAppEngine",
-    urlPatterns = {"/hello"}
-)
+		name = "HelloAppEngine",
+		urlPatterns = {"/hello"}
+		)
 
-//static final serialVerionUID = "10";
 
 public class HelloAppEngine extends HttpServlet {
 
-  /**
-   * TODO change this	
-   * */
-	private static final long serialVersionUID = 1L;
+	/**
+	 * TODO change this	
+	 * */
+	private static final long serialVersionUID = 12L;
 
-	 /** Global instance of the HTTP transport. */
-	  private static HttpTransport httpTransport = null;
-	  private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-	  
-	  /** OAuth 2.0 scopes. */
-	  private static final List<String> SCOPES = Arrays.asList(
-	      "r_basicprofile",
-	      "r_emailaddress");
-	  
-	  /**
-	   * Global instance of the {@link DataStoreFactory}. The best practice is to make it a single
-	   * globally shared instance across your application.
-	   */
-	  private static FileDataStoreFactory dataStoreFactory;
-	  
-	  /** Directory to store user credentials. */
-	  private static final java.io.File DATA_STORE_DIR =
-	      new java.io.File(System.getProperty("user.home"), ".store/oauth2_sample");
-	  
-@Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) 
-      throws IOException {
-	try {
-		httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-	} catch (GeneralSecurityException | IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
+	private static String callbackURI = "http://localhost:8080/login/oauth/mytest/callback";
+	private static String linkedInAuthorizationURI = "https://www.linkedin.com/oauth/v2/authorization";
 	
-	// set up authorization code flow
-/* 
+	private static String linkedInSerendipifyMeClientId = "<ENTER_YOURS>";
+
+	/*
+	private static HttpTransport httpTransport = null;
+	private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+	 */
+	/** OAuth 2.0 scopes. */
+	private static final List<String> SCOPES = Arrays.asList(
+			"r_liteprofile");
+
+	/*
+	private static FileDataStoreFactory dataStoreFactory;
+	 */
+
+	/*
+	private static final java.io.File DATA_STORE_DIR =
+			new java.io.File(System.getProperty("user.home"), ".store/oauth2_sample");
+	 */
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws IOException {
+		/*
+		try {
+			httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+		} catch (GeneralSecurityException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
     dataStoreFactory = new FileDataStoreFactory(DATA_STORE_DIR);
     GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
         httpTransport, JSON_FACTORY, "client_id", "secret", SCOPES).setDataStoreFactory(
         dataStoreFactory).build();
-	 
-	Credential ctoken = flow.loadCredential("lean.sheann");
-	
+
 	String authUrl = flow.newAuthorizationUrl().setState("xyz")
 	        .setRedirectUri("https://localhost:8080/success.html").build();
 	response.getWriter().print("Hello App Engine!\r\n");
-	*/
-	
-//    GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(transport, jsonFactory, client_id, client_secret, scopes).build();
-//    GoogleAuthorizationCodeRequestUrl url = flow.newAuthorizationUrl();
-	
-    String url =
-            new GoogleAuthorizationCodeRequestUrl("https://www.linkedin.com/oauth/v2/authorization",
-                "clientid", "http://localhost:8080/success.html", SCOPES).setState("/profile").build();
-    response.sendRedirect(url);
-//	GoogleAuthorizationCodeRequestUrl(String authorizationServerEncodedUrl, String clientId, String redirectUri, Collection<String> scopes) 
-/*    response.setContentType("text/plain");
-    response.setCharacterEncoding("UTF-8");
-*/
-	
-	
-  }
+    //GoogleAuthorizationCodeRequestUrl url = flow.newAuthorizationUrl();
+		 */
+
+		String accessState = UUID.randomUUID().toString().replace("-", "");
+
+		String url =
+				new GoogleAuthorizationCodeRequestUrl(linkedInAuthorizationURI,
+						linkedInSerendipifyMeClientId, callbackURI, SCOPES).setState(accessState).build();
+
+		response.sendRedirect(url);	
+	}
 }
