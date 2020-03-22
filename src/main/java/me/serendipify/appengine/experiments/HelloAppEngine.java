b013ffc39ad4bfc3b60f71c37db28d7b3f76c55d
ValentinMes <valentin.mesaros@gmail.com>
2020-03-22 21:52:08 +0100

    add servelt for google

--- src/main/java/me/serendipify/appengine/experiments/HelloAppEngine.java
+++ src/main/java/me/serendipify/appengine/experiments/HelloAppEngine.java
@@ -43,9 +43,14 @@ public class HelloAppEngine extends HttpServlet {
 	 * */
 	private static final long serialVersionUID = 12L;
 
+	/*
 	private static String callbackURI = "http://localhost:8080/login/oauth/mytest/callback";
 	private static String linkedInAuthorizationURI = "https://www.linkedin.com/oauth/v2/authorization";
+	private static String linkedInSerendipifyMeClientId = "<ENTER_YOURS>";
+*/
 	
+	private static String callbackURI = "http://localhost:8080/login/oauth/google/callback";
+	private static String linkedInAuthorizationURI = "https://accounts.google.com/o/oauth2/v2/auth";
 	private static String linkedInSerendipifyMeClientId = "<ENTER_YOURS>";
 
 	/*
@@ -53,8 +58,8 @@ public class HelloAppEngine extends HttpServlet {
 	private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
 	 */
 	/** OAuth 2.0 scopes. */
-	private static final List<String> SCOPES = Arrays.asList(
-			"r_liteprofile");
+//	private static final List<String> SCOPES = Arrays.asList("r_liteprofile");
+	private static final List<String> SCOPES = Arrays.asList("email");
 
 	/*
 	private static FileDataStoreFactory dataStoreFactory;
